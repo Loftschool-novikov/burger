3f69181e520a3d37c925e4a634c90c7abf17c730
Loftschool-novikov <novikov.andrey93@gmail.com>
2020-03-23 20:58:07 +0300

    Ховер и подклюил js

---
+++ main.js
@@ -0,0 +1,7 @@
+const humburgerMenu = document.querySelector(".header__burgerMenu");
+const menu = document.querySelector(".menu");
+
+humburgerMenu.addEventListener("click", function() {
+  menu.classList.toggle("menu__hamburger");
+  humburgerMenu.classList.toggle("header__burgerMenu-сlose");
+});
